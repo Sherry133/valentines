d7f8f51f0f1a12ac2620a5dd70ddc4c6b83a1094
Sherry Avena <sherry@idebit.com>
2022-01-09 11:00:39 -0800

    updated alt text

--- assets/js/app1.js
+++ assets/js/app1.js
@@ -4,36 +4,37 @@ const modalBtn = document.querySelector('#modal-btn');
 const closeBtn = document.querySelector('.close');
 
 // Events
-modalBtn.addEventListener('click', function (openModal) {
+modalBtn.addEventListener('click', () => {
     if (e.target == modal) {
         modal.style.display = 'block';
     }
 })
 
 
-closeBtn.addEventListener('click', function (closeModal) {
-            modal.style.display = 'none';
-        }
+closeBtn.addEventListener('click', function () {
+    modal.style.display = 'none';
+})
 
-        window.addEventListener('click', function (outsideClick) {
-            if (e.target == modal) {
-                modal.style.display = 'none';
-            }
-        })
+window.addEventListener('click', function () {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+})
 
-        // Open
-        // function openModal() {
-        //     modal.style.display = 'block';
+// Open
+function openModal() {
+    modal.style.display = 'block';
 
-        // }
+}
 
-        // Close
-        // function closeModal() {
-        //     modal.style.display = 'none';
-        // }
+// Close
+function closeModal() {
+    modal.style.display = 'none';
+}
 
-        // Close If Outside Click. Change 'modal' to background of home page of school savings.
-        // function outsideClick(e) {
-        //     if (e.target == modal) {
-        //         modal.style.display = 'none';
-        //     }}
+// Close If Outside Click. Change 'modal' to background of home page of school savings.
+function outsideClick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
